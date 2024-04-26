92cb58e912eaeae2af68a52d773e876e865343be
Tom Mazzag <itstomazz@gmail.com>
2024-04-26 13:09:32 +0100

    debugging

--- src/pages/Navigation/Landing.tsx
+++ src/pages/Navigation/Landing.tsx
@@ -3,51 +3,51 @@ import { refreshToken } from "../../services/token";
 export const Welcome = () => {
     const authorize = async () => {
         try {
-        const generateRandomString = (length: number) => {
-            const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            const values = crypto.getRandomValues(new Uint8Array(length));
-            return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-        }
-        const codeVerifier  = generateRandomString(64);
+            const generateRandomString = (length: number) => {
+                const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                const values = crypto.getRandomValues(new Uint8Array(length));
+                return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+            }
+            const codeVerifier  = generateRandomString(64);
 
-        const sha256 = async (plain: string) => {
-            const encoder = new TextEncoder()
-            const data = encoder.encode(plain)
-            return window.crypto.subtle.digest('SHA-256', data)
-        }
+            const sha256 = async (plain: string) => {
+                const encoder = new TextEncoder()
+                const data = encoder.encode(plain)
+                return window.crypto.subtle.digest('SHA-256', data)
+            }
 
-        const base64encode = (input: any) => {
-            return btoa(String.fromCharCode(...new Uint8Array(input)))
-                .replace(/=/g, '')
-                .replace(/\+/g, '-')
-                .replace(/\//g, '_');
-        }          
+            const base64encode = (input: any) => {
+                return btoa(String.fromCharCode(...new Uint8Array(input)))
+                    .replace(/=/g, '')
+                    .replace(/\+/g, '-')
+                    .replace(/\//g, '_');
+            }          
 
-        const hashed = await sha256(codeVerifier)
-        const codeChallenge = base64encode(hashed);
+            const hashed = await sha256(codeVerifier)
+            const codeChallenge = base64encode(hashed);
 
-        const redirectUri = import.meta.env.VITE_REDIRECT_URI
-        const clientId = import.meta.env.VITE_CLIENT_ID;
+            const redirectUri = import.meta.env.VITE_REDIRECT_URI
+            const clientId = import.meta.env.VITE_CLIENT_ID;
 
 
-        const scope = 'user-read-private user-read-email playlist-read-collaborative playlist-read-private';
-        const authUrl = new URL("https://accounts.spotify.com/authorize")
+            const scope = 'user-read-private user-read-email playlist-read-collaborative playlist-read-private';
+            const authUrl = new URL("https://accounts.spotify.com/authorize")
 
-        console.log(codeVerifier)
-        window.localStorage.setItem('code_verifier', codeVerifier);
+            console.log("VERIFIER: " + codeVerifier)
+            window.localStorage.setItem('code_verifier', codeVerifier);
 
-        const params =  {
-            response_type: 'code',
-            client_id: clientId,
-            scope,
-            code_challenge_method: 'S256',
-            code_challenge: codeChallenge,
-            redirect_uri: redirectUri,
-        }
+            const params =  {
+                response_type: 'code',
+                client_id: clientId,
+                scope,
+                code_challenge_method: 'S256',
+                code_challenge: codeChallenge,
+                redirect_uri: redirectUri,
+            }
 
-        authUrl.search = new URLSearchParams(params).toString();
-        console.log(authUrl)
-        window.location.href = authUrl.toString();
+            authUrl.search = new URLSearchParams(params).toString();
+            console.log(authUrl)
+            window.location.href = authUrl.toString();
         } catch(err) {
             console.error(err)
         }
