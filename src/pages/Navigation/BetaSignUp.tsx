45cf1ef7c377a8525c18ee50283dd787e34bf451
Tom Mazzag <itstomazz@gmail.com>
2024-11-06 19:02:15 +0000

    feat(MFE-17): Created signup form

--- src/pages/Navigation/BetaSignUp.tsx
+++ src/pages/Navigation/BetaSignUp.tsx
@@ -1,11 +1,17 @@
 export const BetaSignUp = () => {
     return (
-        <div className="h-[100vh] flex justify-center align-middle items-center">
-            <div>
-                <p>Sign up for the beta <a href="" className="link">here</a></p>
+        <div className="h-[100vh] flex justify-center align-middle items-center text-center">
+            <div className="md:w-[400px]">
+                <p>Sign up for the beta here</p>
 
-                {/* Signup form coming */}
+                <form action="https://getlaunchlist.com/s/POQK9Z" method="POST" className="mt-10 flex flex-col gap-2">
+                    <input name="name" type="text" className="input input-bordered w-full" placeholder="Full Name"></input>
+                    <input name="email" type="email" className="input input-bordered w-full" placeholder="Email"></input>
+                    <button type="submit" className="btn btn-primary mt-2">
+                        Sign Up
+                    </button>
+                </form>
             </div>
         </div>
-    )
+    );
 };
